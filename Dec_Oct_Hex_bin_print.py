ca113eac8d9311e332b3b7782f0d846a61bfe5e5
Roushan022 <roushan.akhtar_bca25@theaims.ac.in>
2026-07-18 19:52:57 +0530

    Dec_Oct_Hex_bin_print.py

---
+++ Dec_Oct_Hex_bin_print.py
@@ -0,0 +1,12 @@
+def print_formatted(number):
+    width=len(bin(number))-2
+    for i in range(1,number+1):
+        dec=str(i)
+        octal_num=oct(i)[2:]
+        hex_num=hex(i)[2:]
+        Binar_num=bin(i)[2:]
+        print(f"{dec:>{width}} { octal_num:>{width}} { hex_num:>{width}} {Binar_num:>{width}}")
+return
+
+n=int(input("Enter a number"))
+
