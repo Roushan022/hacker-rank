95ed41249c3f5888843a4e1262796e7628a157eb
Roushan022 <roushan.akhtar_bca25@theaims.ac.in>
2026-07-25 11:06:14 +0530

    17_math_print.py

---
+++ 17_math_print.py
@@ -0,0 +1,12 @@
+for i in range(1,input()):
+  print(((10**i-1)//9)*i) # or if we want to print in integer format
+  # or if we want to print in str format
+  c=str(i)
+  print(c*i)
+
+#output 
+1
+22
+333
+4444
+  
