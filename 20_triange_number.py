332de214bb9e508887f5ac6d5d8145d09e614aee
Roushan022 <roushan.akhtar_bca25@theaims.ac.in>
2026-07-26 13:56:57 +0530

    20_triange_number.py

--- 20_triange_number.py
+++ 20_triange_number.py
@@ -6,3 +6,9 @@
 for i in range(1, 6):
     print(''.join(str(j) for j in range(1, i + 1)) + ''.join(str(j) for j in range(i - 1, 0, -1)))
   
+for i in range(1, 6):
+    for j in range(1,i+1):
+        print(j,end="",)
+    for j in range(i - 1, 0, -1):
+        print(j, end="")
+    print()
