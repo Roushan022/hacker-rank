5c9f138b3c43b9daba782430e8dbc66dbffea028
Roushan022 <roushan.akhtar_bca25@theaims.ac.in>
2026-07-27 19:18:46 +0530

    Create 22_pow.py

---
+++ 22_pow.py
@@ -0,0 +1,7 @@
+# Read four numbers,a ,b ,c , and d,nd print the result of a to the power of b and c to the pow of d .
+import math
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+print(pow(a,b)+pow(c,d))
